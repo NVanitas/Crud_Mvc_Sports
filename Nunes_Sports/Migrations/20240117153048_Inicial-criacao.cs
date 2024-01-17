eaf228d988b374badf29c629a680947af5244b87
NVanitas <nicolasdasilvasantos04@gmail.com>
2024-01-17 14:06:35 -0300

    Publicação

---
+++ Nunes_Sports/Migrations/20240117153048_Inicial-criacao.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nunes_Sports.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicialcriacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos Sports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Preco = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos Sports", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos Sports");
+        }
+    }
+}
---
+++ Nunes_Sports/Models/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Nunes_Sports.Models
+{
+    [Table("Produtos Sports")]
+    public class Produto
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public String Nome { get; set; }
+
+        [Column("Descricao")]
+        [Display(Name = "Descrição")]
+        public String Descricao { get; set; }
+
+        [Column("Preco")]
+        [Display(Name = "Preço")]
+        public double Preco { get; set; }
+    }
+}
